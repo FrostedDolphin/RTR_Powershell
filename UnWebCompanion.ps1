6b53a8db762c3eb7fd54b8f669a34f8680044913
Austin Moeller <71355557+FrostedDolphin@users.noreply.github.com>
2024-10-02 07:51:42 -0700

    Update UnWebCompanion.ps1

--- UnWebCompanion.ps1
+++ UnWebCompanion.ps1
@@ -1,73 +1,109 @@
 $badreg = 'Registry::HKU\*\Software\Microsoft\Windows\CurrentVersion\Run'
-$malfiledir = "C:\Program Files (x86)\Lavasoft\Web Companion\", "C:\Users\*\AppData\Roaming\BBWC\", "C:\Users\*\AppData\Roaming\Browser Extension\updater.exe"
-#Idea:
+$malfiledir = "C:\Program Files (x86)\Lavasoft\Web Companion\", "C:\Users\*\AppData\Roaming\BBWC\", "C:\Users\*\AppData\Roaming\Browser Extension\updater.exe", "C:\Users\*\AppData\Roaming\Lavasoft\Web Companion\"
 #$updaterhash="<SHA-256>"
 #get hash of updater.exe and delete file by known bad hash
 
-echo '---------------------------------------';
 
-echo 'Registry Run Persistence Being Removed:'
+Write-Output "Removed:"
 
-echo '---------------------------------------'; 
 Foreach ($reg in $badreg)
 {
-$regoutput1 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'WCStartup'} | select -exp Property ;
-$regoutput2 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'WCUpate'} | select -exp Property ;
-$regoutput3 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'WCEStartup'} | select -exp Property ;
-$regoutput4 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'WCEUpdater'} | select -exp Property ;
-$regoutput5 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'Web Companion'} | select -exp Property ;
+    $itemsFound = $false
 
+    $regoutput1 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'WCStartup'} | select -exp Property ;
+    $regoutput2 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'WCUpate'} | select -exp Property ;
+    $regoutput3 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'WCEStartup'} | select -exp Property ;
+    $regoutput4 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'WCEUpdater'} | select -exp Property ;
+    $regoutput5 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'Web Companion'} | select -exp Property ;
+    $regoutput6 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'Ad-Aware Search Companion'} | select -exp Property ;
 
-$regpath1 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'WCStartup'} | select -exp Name ;
-$regpath2 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'WCUpate'} | select -exp Name ;
-$regpath3 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'WCEStartup'} | select -exp Name ;
-$regpath4 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'WCEUpdater'} | select -exp Name ;
-$regpath5 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'Web Companion'} | select -exp Name ;
+    $regpath1 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'WCStartup'} | select -exp Name ;
+    $regpath2 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'WCUpate'} | select -exp Name ;
+    $regpath3 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'WCEStartup'} | select -exp Name ;
+    $regpath4 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'WCEUpdater'} | select -exp Name ;
+    $regpath5 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'Web Companion'} | select -exp Name ;
+    $regpath6 = gi -path $reg -ea silentlycontinue | ? {$_.Property -like 'Ad-Aware Search Companion'} | select -exp Name ;
 
-Foreach($prop1 in $regoutput1){
-If ($prop1 -like 'WCStartup') {"$regpath1 value: $prop1 `n"
-reg delete $regpath1 /v $prop1 /f
-}}
+    Foreach($prop1 in $regoutput1){
+        If ($prop1 -like 'WCStartup') {
+            Write-Output "$regpath1 value: $prop1"
+            reg delete $regpath1 /v $prop1 /f
+            $itemsFound = $true
+        }
+    }
 
-Foreach($prop2 in $regoutput2){
-If ($prop2 -like 'WCUpate') {"$regpath2 value: $prop2 `n"
-reg delete $regpath2 /v $prop2 /f
-}}
+    Foreach($prop2 in $regoutput2){
+        If ($prop2 -like 'WCUpate') {
+            Write-Output "$regpath2 value: $prop2"
+            reg delete $regpath2 /v $prop2 /f
+            $itemsFound = $true
+        }
+    }
 
-Foreach($prop3 in $regoutput3){
-If ($prop3 -like 'WCEStartup') {"$regpath3 value: $prop3 `n"
-reg delete $regpath3 /v $prop3 /f
-}}
+    Foreach($prop3 in $regoutput3){
+        If ($prop3 -like 'WCEStartup') {
+            Write-Output "$regpath3 value: $prop3"
+            reg delete $regpath3 /v $prop3 /f
+            $itemsFound = $true
+        }
+    }
 
-Foreach($prop4 in $regoutput4){
-If ($prop4 -like 'WCEUpdater') {"$regpath4 value: $prop4 `n"
-reg delete $regpath4 /v $prop4 /f
-}}
+    Foreach($prop4 in $regoutput4){
+        If ($prop4 -like 'WCEUpdater') {
+            Write-Output "$regpath4 value: $prop4"
+            reg delete $regpath4 /v $prop4 /f
+            $itemsFound = $true
+        }
+    }
 
-Foreach($prop5 in $regoutput5){
-If ($prop5 -like 'Web Companion') {"$regpath5 value: $prop5 `n"
-reg delete $regpath5 /v $prop5 /f
-}}
-    
+    Foreach($prop5 in $regoutput5){
+        If ($prop5 -like 'Web Companion') {
+            Write-Output "$regpath5 value: $prop5"
+            reg delete $regpath5 /v $prop5 /f
+            $itemsFound = $true
+        }
+    }
+
+    Foreach($prop6 in $regoutput6){
+        If ($prop6 -like 'Ad-Aware Search Companion') {
+            Write-Output "$regpath6 value: $prop6"
+            reg delete $regpath6 /v $prop6 /f
+            $itemsFound = $true
+        }
+    }
+
+    if (-not $itemsFound) {
+        Write-Output "no reg items"
+    }
 }
 
-echo '-----------------------------------';
+$stasks = schtasks /query /fo csv /v | convertfrom-csv | ?{$_.TaskName -like 'WC* ScheduledTask'} | select -exp TaskName
 
-echo 'Files in BBWC Folder Being Removed:'
 
-echo '-----------------------------------'; 
+if ($stasks){
 
-get-childitem -Recurse $malfiledir
-remove-Item $malfiledir -Recurse -Force
+Foreach ($task in $stasks){
 
-echo '----------------------------------------------------'; 
-echo "WebCompanion files eliminated." 
-echo "remove related msi file(s) and other bundled adware."
-echo "Heres a list of Chrome extensions to review.."
-echo '----------------------------------------------------'; 
-echo `n
+Write-Output "SchTask- "$task
 
-#check for browser extension names in Chrome
+schtasks /delete /tn $task /F
+
+}
+
+}
+
+else {Write-Output"No Scheduled Tasks"};
+
+$files = get-childitem -Recurse $malfiledir -ea silentlycontinue
+
+if ($files) {
+    remove-Item $malfiledir -Recurse -Force -ea silentlycontinue
+} else {
+    Write-Output "No files"
+}
+
+Write-Output "Chrome Extensions:"
+#check for browser extension names
 function ConvertFrom-Json20([object] $item)
 {
     add-type -assembly system.web.extensions
@@ -76,6 +112,7 @@ function ConvertFrom-Json20([object] $item)
 }
  
 $output = 'User, Extension Name, Version' + [System.Environment]::NewLine
+$extensionsFound = $false
 $users = Get-ChildItem c:\users | ?{ $_.PSIsContainer }
 foreach ($user in $users)
 {
@@ -88,7 +125,13 @@ foreach ($user in $users)
         if (-Not ($json.name -like '__msg_*'))
         {  
             $output += $user.Name + ', ' + $json.name + ', ' + $json.version + [System.Environment]::NewLine
+            $extensionsFound = $true
         }
     }
 }
-$output
+
+if (-not $extensionsFound) {
+    Write-Output "no extensions"
+} else {
+    $output
+}
